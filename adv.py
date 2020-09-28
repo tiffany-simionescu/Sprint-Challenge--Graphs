503629098d5b937ce0de4df372d0e83d5df40bad
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-09-27 20:20:31 -0400

    Initial Commit - Added Queue class

--- adv.py
+++ adv.py
@@ -5,6 +5,23 @@ from world import World
 import random
 from ast import literal_eval
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 # Load world
 world = World()
 
